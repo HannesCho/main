caf429aa61e9dc0a18226fa18804125ff0efa19d
Hannes Cho <hoonsungcho11@gmail.com>
2022-04-01 18:44:26 +0200

    small title text

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,6 +6,13 @@ const divStyle = {
     "linear-gradient(110deg, #6C757D, #6C757D 50%, #212529 50%, #212529)",
 };
 
+const textStyle = {
+  letterSpacing: "1rem",
+};
+
+const smallTitles =
+  "mt-4 pt-3 pb-3 ps-3 text-center text-uppercase fs-2 fw-bold border border-dark border-4";
+
 function Home() {
   return (
     <Container fluid className="vw-100 p-0 m-0 bg-secondary">
@@ -33,17 +40,23 @@ function Home() {
       </Row>
       <Row fluid="md" className="m-0 d-flex flex-row align-items-center vh-100">
         <Col className="d-flex flex-column align-items-center vh-100">
-          About me
+          <span className={smallTitles} style={textStyle}>
+            About Me
+          </span>
         </Col>
       </Row>
       <Row fluid="md" className="m-0 d-flex flex-row align-items-center vh-100">
         <Col className="d-flex flex-column align-items-center vh-100">
-          Skills
+          <span className={smallTitles} style={textStyle}>
+            Skills
+          </span>
         </Col>
       </Row>
       <Row fluid="md" className="m-0 d-flex flex-row align-items-center vh-100">
         <Col className="d-flex flex-column align-items-center vh-100">
-          Portfolio
+          <span className={smallTitles} style={textStyle}>
+            Portfolio
+          </span>
         </Col>
       </Row>
     </Container>
