599febd25aa0a6e91b9d18facbf33f4e8fddfd11
Hannes Cho <123factory@Hoonsungs-MacBook-Air.local>
2024-01-17 21:20:15 +0100

    fix address

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route
-            path="/main"
+            path="/"
             element={
               <Layout>
                 <Home />
